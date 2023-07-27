c97f501e69aa2cd94a65f356063b46504c2b73c2
Yog <yog.panjarale@gmail.com>
2023-07-28 03:18:49 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,114 @@
-import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
-
-const router = new Router();
-router
-  .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
-  })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
-  });
+import { Application, Router, Context } from "https://deno.land/x/oak/mod.ts";
+import { MongoClient } from "https://deno.land/x/mongo/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const app = new Application();
-app.use(oakCors()); // Enable CORS for All Routes
+const router = new Router();
+
+const {
+  MONGODB_URI,
+  MONGODB_DB,
+  DISCORD_WEBHOOK_URL,
+  PORT = "3000",
+} = config();
+
+const client = new MongoClient();
+await client.connect({
+  db: MONGODB_DB,
+  uri: MONGODB_URI,
+});
+
+const db = client.database(MONGODB_DB);
+const collection = db.collection("counters");
+
+interface Counter {
+  _id: { $oid: string };
+  key: string;
+  count: number;
+}
+
+const PLACES = 7;
+
+export function makeSVG(count: number) {
+  const countArray = count.toString().padStart(PLACES, "0").split("");
+  const parts = countArray.reduce(
+    (acc, next, index) => `
+        ${acc}
+        <rect id="Rectangle" fill="#000000" x="${index *
+      32}" width="29" height="29"></rect>
+        <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill="#00FF13">
+            <tspan x="${index * 32 + 7}" y="22">${next}</tspan>
+        </text>
+`,
+    ""
+  );
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <svg width="${PLACES *
+    32}px" height="30px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>Count</title>
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        ${parts}
+      </g>
+  </svg>
+  `;
+};
+
+router.get("/:key/count", async (context: Context) => {
+  try {
+    const { key } = context.params;
+    const counter: Counter | null = await collection.findOne({ key });
+
+    if (counter) {
+      // Increment the counter
+      const updatedCount = counter.count + 1;
+      await collection.updateOne(
+        { _id: counter._id },
+        { $set: { count: updatedCount } }
+      );
+
+      // Send message to Discord via webhook
+      await fetch(DISCORD_WEBHOOK_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          content: `Hit count for ${key}: ${updatedCount}`,
+        }),
+      });
+
+      // Create SVG
+      const svg = makeSVG(updatedCount);
+
+      context.response.headers.set("Content-Type", "image/svg+xml");
+      context.response.body = svg;
+    } else {
+      // If the counter does not exist, create a new one
+      const newCount = 1;
+      await collection.insertOne({ key, count: newCount });
+
+      // Send message to Discord via webhook
+      await fetch(DISCORD_WEBHOOK_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          content: `Hit count for ${key}: ${newCount}`,
+        }),
+      });
+
+      // Create SVG
+      const svg = makeSvg(newCount);
+
+      context.response.headers.set("Content-Type", "image/svg+xml");
+      context.response.body = svg;
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    context.response.status = 500;
+    context.response.body = "Internal Server Error";
+  }
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+console.log(`Server is running on port ${PORT}`);
+await app.listen({ port: parseInt(PORT) });
